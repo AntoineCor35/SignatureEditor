3ab44a0a7f4257fef5cad44133aca576092c06bc
AntoineCor35 <antoine.cormier@my-digital-school.org>
2025-03-07 13:58:12 +0100

    Initial Commit

---
+++ SignatureEditor/SignatureEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignatureEditorApp.swift
+//  SignatureEditor
+//
+//  Created by Antoine Cormier on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SignatureEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
